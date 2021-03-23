742897a1cf7adc97c57f45cf4318b84c19a97f8f
Julia Nikolaeva <iamenates@gmail.com>
2021-03-23 19:48:47 +0300

    added object personalMovieDB

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
 let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: '',
+    actors: '',
+    genres: [],
+    privat: false,
+}
+
+//console.log(personalMovieDB);
